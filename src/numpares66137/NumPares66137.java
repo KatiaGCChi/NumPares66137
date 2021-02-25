807254393781f83240e95c3abc20eadf1c3439db
TRUJEQUE <TRUJEQUE@ANDREA>
2021-02-24 20:49:41 -0600

    numpares

---
+++ src/numpares66137/NumPares66137.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares66137;
+
+/**
+ *
+ * @author TRUJEQUE
+ */
+public class NumPares66137 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int b = 0;
+        while (b < 94);
+            if (b % 2 == 0);
+            System.out.println("b = " + b);
+                
+                
+            b++;
+    }
+    
+}
